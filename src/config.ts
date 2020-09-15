ffeef272471f949f18b0b3c1fcfb46646fcd3849
sunrise1115 <sunrise1115@outlook.com>
2020-09-16 03:34:01 +0800

    removed jwt module

--- src/config.ts
+++ src/config.ts
@@ -12,11 +12,4 @@ export const db = {
 
 export const corsUrl = process.env.CORS_URL;
 
-export const tokenInfo = {
-  accessTokenValidityDays: parseInt(process.env.ACCESS_TOKEN_VALIDITY_DAYS),
-  refreshTokenValidityDays: parseInt(process.env.REFRESH_TOKEN_VALIDITY_DAYS),
-  issuer: process.env.TOKEN_ISSUER,
-  audience: process.env.TOKEN_AUDIENCE,
-};
-
 export const logDirectory = process.env.LOG_DIR;
